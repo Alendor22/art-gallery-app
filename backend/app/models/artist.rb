78e7fec1c1305ad1ae3bcdd8f990a652d99c7fae
Adeja <alendor6@gmail.com>
2020-04-11 19:42:30 -0400

    changed the relationship setup

--- backend/app/models/artist.rb
+++ backend/app/models/artist.rb
@@ -1,6 +1,6 @@
 class Artist < ApplicationRecord
 
-  has_many :paintings
-  has_many :paintings, through: :art_gallery
+  has_many :art_galleries
+  has_many :paintings, through: :art_galleries
 
 end
