9cd1eb3d6f81ee6b662c1e4c4a9213b9b8f87f5a
Adeja <alendor6@gmail.com>
2020-04-11 16:53:07 -0400

    created painting migration

---
+++ backend/db/migrate/20200411193913_create_paintings.rb
@@ -0,0 +1,12 @@
+class CreatePaintings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :paintings do |t|
+      t.string :title
+      t.text :caption
+      t.string :style
+      t.string :price
+
+      t.timestamps
+    end
+  end
+end
