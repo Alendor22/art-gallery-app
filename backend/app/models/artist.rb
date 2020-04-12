d565e85447233c723e8ae1644ca67eddc29ea763
Adeja <alendor6@gmail.com>
2020-04-12 13:23:23 -0400

    added painting_url method

--- backend/app/models/artist.rb
+++ backend/app/models/artist.rb
@@ -2,6 +2,12 @@ class Artist < ApplicationRecord
 
   has_many :art_galleries
   has_many :paintings, through: :art_galleries
-  has_one_attached :painting
+  has_one_attached :painting_image
+
+  def painting_url
+    if self.painting_image.attachment
+      self.painting_image.attachment.service_url
+    end
+  end
 
 end
