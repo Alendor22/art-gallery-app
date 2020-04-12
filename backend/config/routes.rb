f95ad3138701a85c301b72411a1e23a9542decf7
Adeja <alendor6@gmail.com>
2020-04-12 11:27:19 -0400

    created namespacing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :paintings
-  resources :artists
+  namespace :api do
+    namespace :v1 do
+      resources :paintings
+      resources :artists
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
