4565b1b914e2ac457b56d2c2d79796dfec342b43
Adeja <alendor6@gmail.com>
2020-04-18 14:22:18 -0400

    deleted before_save from model

--- backend/app/models/artist.rb
+++ backend/app/models/artist.rb
@@ -3,5 +3,12 @@ class Artist < ApplicationRecord
   has_many :paintings
   
   validates :name, :age, :gender, presence: true
+  before_save :create_painting
+
+  # AIzaSyCrp_bpCBRLt-viXwiHd21RsliqDE7e5Ag
   
+  def create_painting
+  end
+
+
 end
