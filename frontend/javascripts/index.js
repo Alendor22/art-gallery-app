02ef457ea3cd9f56df42689167df06e7a9d778f7
Adeja <alendor6@gmail.com>
2020-04-18 14:24:23 -0400

    added post artist to painting

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const getArtistForm = () => document.querySelector("#artist-form");
 const getPaintingForm = () => document.querySelector("#insert-painting");
 const getDeleteButton = () => document.querySelectorAll('.artist-card button');
-const deleteButtons = getDeleteButton();
+
 
 const getTitle = () => getPaintingForm().querySelector("#title").value;
 const getStyle = () => getPaintingForm().querySelector("#style").value;
@@ -18,8 +18,33 @@ const getAge = () => getArtistForm().querySelector('#age').value;
 const getGender = () => getArtistForm().querySelector('#gender').value;
 
 function listenForClick() {
-  paintingForm.addEventListener("submit", (e) => {
+  getPaintingForm().addEventListener("submit", (e) => {
     e.preventDefault();
+  
+  let title = getTitle();
+  let style = getStyle();
+  let price = getPrice();
+  let url = document.querySelector('#url').value
+  let artist_id = document.querySelector("#artist-select").value
+
+  let strongParams = {
+    painting: { title, style, price, url, artist_id }
+  };
+
+  fetch('http://localhost:3000/api/v1/paintings', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(strongParams)
+   })
+    .then(resp => resp.json())
+    .then(data => {
+      console.log(data)
+      let painting = new Painting(data)
+      renderPaintingFromTemplate(paintingTemplate(painting));
+    })
   });
 }
 
@@ -78,6 +103,7 @@ getArtistForm().addEventListener('submit', createArtistFromForm);
 
       renderArtistFromTemplate(artistTemplate(artist));
       
+      let deleteButtons = getDeleteButton()
       let lastButton = deleteButtons.length-1;
       let deleteButton = deleteButtons[lastButton];
 
@@ -90,67 +116,76 @@ getArtistForm().addEventListener('submit', createArtistFromForm);
     .catch(error => {
       alert(error)
     })
-  }
+}
 
   function renderArtistFromTemplate(artistTemplate) {
     document.querySelector('#artists').innerHTML += artistTemplate;
   }
 
   function artistTemplate(artist) {
-    // debugger
     return `
         <div class="artist-card">
           <div class="artist-card-content">
             <P>Artist Name: ${artist.name}</P>
             <p>Artist Age: ${artist.age}</p>
-            <p>Artist Gender: ${artist.gender}</p> 
+            <p>Artist Gender: ${artist.gender}</p>
+            <p>Painting: ${artist.paintings}</p>
             <button id="${artist.id}">Delete Artist:</button>
           </div>
         </div>
       `;
   }
+  
 
   async function loadArtists() {
     let response = await fetch("http://localhost:3000/api/v1/artists");
     let artists = await response.json();
 
     artists.forEach((data) => {
-    let artist = new Artist(data);
+      let artist = new Artist(data);
     renderArtistFromTemplate(artistTemplate(artist))
     })
+
+    // Adding a dropdown to select an artist from existing artists
+    // wait till artists are created
+    // add a prototype method || static method iterate through the artist to build the options for the select
+    // target form and add this input(select)
+    // attach artist id to each option
+    // when a fetch request is made make sure they the artwork is associated to an artist
+    Artist.addArtistsToSelectDropDown()
     
+    listenForClick()
+
     deleteAction()
   
   }
 
-  function deleteAction(){  
+  function deleteAction(){ 
+    let deleteButtons = getDeleteButton() 
     for(let i = 0; i < deleteButtons.length; i++){
       deleteButtons[i].addEventListener('click', deleteArtistFromForm)
     }
-    
-    /*function deleteArtistFromForm(e) {
-      e.preventDefault()
-    }*/
 
   }
 
-  function deleteArtistFromForm(e) {
-    const id = e.target // maybe .id
-    
-    e.preventDefault()
+  function deleteArtistFromForm() {
+    let id = this.id
+    deleteArtist(id);
+  }
   
+  function deleteArtist(id) {
     fetch(`http://localhost:3000/api/v1/artists/${id}`, {
     method: 'DELETE'
     
     })
     .then(resp => resp.json())
     .then(artist => {
-      Artist.all.pop(artist);
-
-      renderArtistFromTemplate(artistTemplate());
+      document.querySelector('#artists').innerHTML = ""
+      Artist.all = []
+      loadArtists()
     })
-  } 
-
+   
+  }
 /*async function loadArtists() {
   let response = await fetch("http://localhost:3000/api/v1/artists");
   let artists = await response.json();
