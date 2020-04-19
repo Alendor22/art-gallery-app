acacd106eeacff70d313f1b3ad3144fd93fee18b
Adeja <alendor6@gmail.com>
2020-04-18 23:01:05 -0400

    added sort_paintings scope method

--- backend/app/models/painting.rb
+++ backend/app/models/painting.rb
@@ -3,5 +3,7 @@ class Painting < ApplicationRecord
   belongs_to :artist
 
   validates :title, :style, :price, presence: true
+
+  scope :sort_paintings, -> {order('paintings.artist_id desc')}
   
 end
