af815d54807c558e6ac27bc0e42c8b7e629dd825
Adeja <alendor6@gmail.com>
2020-04-22 22:28:00 -0400

    add get\post\delete methods

--- frontend/javascripts/services/api.js
+++ frontend/javascripts/services/api.js
@@ -3,17 +3,34 @@ class API {
   static baseURL = 'http://localhost:3000/api';
 
   static get(url) {
-    return fetch(url)
-    return let response = await fetch(this.baseURL + url)
-    let paintings = await response.json();
-  
-    paintings.forEach((data) => {
-      let painting = new Painting(data);
-      Painting.renderPaintingFromTemplate(painting.paintingTemplate(painting));
-    });
-    this.deletePaintingAction
+      return fetch(API.baseURL + url)
+        .then(function (response) {
+          if (response.status !== 200) {
+            throw new Error(response.statusText)
+          }
+          return response.json();
+        });
   }
 
+  static post(url, data) {
+     return fetch(this.baseURL + url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+     })
+      .then(resp => resp.json());
+  }
+
+  static delete(url) {
+    return fetch(API.baseURL + url, {
+      method: 'DELETE'
+    })
+    .then(resp => resp.json());
+  }
+    
 
 
 }
